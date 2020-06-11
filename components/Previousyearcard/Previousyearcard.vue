4ae2ed6e65d30f6fe0d094a1df78f01d257851ac
Prince Rajput <drprincerajput@gmail.com>
2020-06-11 13:19:19 +0530

    check updating real time

--- components/Previousyearcard/Previousyearcard.vue
+++ components/Previousyearcard/Previousyearcard.vue
@@ -4,46 +4,103 @@
       <div>
         <b-card-group deck>
           <b-card bg-variant="light" title="Primary" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-            <b-button href="#" variant="primary">Go somewhere</b-button>
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
+            <b-button href="#" variant="primary">Previous Year</b-button>
           </b-card>
 
-          <b-card bg-variant="secondary" text-variant="white" title="Secondary" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          <b-card
+            bg-variant="secondary"
+            text-variant="white"
+            title="Secondary"
+            class="text-center"
+          >
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
           </b-card>
 
-          <b-card bg-variant="success" text-variant="white" title="Success" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          <b-card
+            bg-variant="success"
+            text-variant="white"
+            title="Success"
+            class="text-center"
+          >
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
           </b-card>
         </b-card-group>
       </div>
       <div class="mt-3">
         <b-card-group deck>
-          <b-card bg-variant="info" text-variant="white" title="Info" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          <b-card
+            bg-variant="info"
+            text-variant="white"
+            title="Info"
+            class="text-center"
+          >
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
           </b-card>
 
-          <b-card bg-variant="warning" text-variant="white" title="Warning" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          <b-card
+            bg-variant="warning"
+            text-variant="white"
+            title="Warning"
+            class="text-center"
+          >
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
           </b-card>
 
-          <b-card bg-variant="danger" text-variant="white" title="Danger" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          <b-card
+            bg-variant="danger"
+            text-variant="white"
+            title="Danger"
+            class="text-center"
+          >
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
           </b-card>
         </b-card-group>
       </div>
       <div class="mt-3">
         <b-card-group deck>
           <b-card bg-variant="light" title="Light" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
           </b-card>
 
-          <b-card bg-variant="dark" title="Dark" text-variant="white" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          <b-card
+            bg-variant="dark"
+            title="Dark"
+            text-variant="white"
+            class="text-center"
+          >
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
           </b-card>
 
           <b-card title="Default" class="text-center">
-            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            <b-card-text
+              >Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit.</b-card-text
+            >
           </b-card>
         </b-card-group>
       </div>
@@ -55,5 +112,4 @@
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
